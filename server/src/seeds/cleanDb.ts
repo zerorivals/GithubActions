795c9429f37eb3e2277c1b2828ddd7db7c56d64d
zerorivals <z_haskell98@outlook.com>
2025-04-25 18:32:08 -0700

    fixed files

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,11 +3,11 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
+    let modelExists = await models[modelName]?.db?.db?.listCollections({
       name: collectionName
     }).toArray()
 
-    if (modelExists.length) {
+    if (modelExists?.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -2,7 +2,7 @@ import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
 
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
+import pythonQuestions from './pythonQuestions.json' with { type: "json" };
 
 db.once('open', async () => {
   await cleanDB('Question', 'questions');
